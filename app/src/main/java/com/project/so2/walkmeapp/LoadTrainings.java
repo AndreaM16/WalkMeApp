6565a26d8bf7a43b1ec3a17d197e67c05ed5b49b
AndreaM16 <andrea.medda@live.it>
2016-01-25 22:04:30 +0100

    Added: Load Trainings Class, Strings, Back Button, Action Bar with Back, Action Bar with Back and Dynamic Title

--- app/src/main/java/com/project/so2/walkmeapp/LoadTrainings.java
+++ app/src/main/java/com/project/so2/walkmeapp/LoadTrainings.java
@@ -1,22 +1,26 @@
 package com.project.so2.walkmeapp;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 
 /**
  * Created by Andrea on 24/01/2016.
  */
-public class LoadTrainings extends MainActivity{
+public class LoadTrainings extends Activity{
 
     private String[] mLoadTrainingsElements;
-    //private DrawerLayout mLoadTrainingsLayout;
 
     @Override
+    //Bundle -> Allows passing values between Activities
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Binding Class to its View
         setContentView(R.layout.load_trainings);
 
+        //Binding Strings to their View
         mLoadTrainingsElements = getResources().getStringArray(R.array.load_trainings_list_items);
-        //mLoadTrainingsLayout = (LoadTrainingsLayout) findViewById(R.id.load_trainings);
+
     }
 }
