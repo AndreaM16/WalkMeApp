7ac387a18b4b4db5f89ff643f88ae701ea563aa8
Loriz <doppiomauro@gmail.com>
2016-01-29 16:48:47 +0100

    First rework of MainPage

--- app/src/main/java/com/project/so2/walkmeapp/ui/MainActivity.java
+++ app/src/main/java/com/project/so2/walkmeapp/ui/MainActivity.java
@@ -3,132 +3,94 @@ package com.project.so2.walkmeapp.ui;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.project.so2.walkmeapp.R;
 
 public class MainActivity extends Activity {
-    private String[] mDrawerElements;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ImageView mMenuIcon;
-    private LinearLayout mMainPageList;
-    private String[] mMainPageElements;
+   private LinearLayout mMainPageList;
+   private String[] mMainPageElements;
+   private ImageView mUserView;
 
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
 
 
-        mDrawerElements = getResources().getStringArray(R.array.drawer_items);
-        mMainPageElements = getResources().getStringArray(R.array.main_page_list_items);
+      mMainPageElements = getResources().getStringArray(R.array.main_page_list_items);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mMainPageList = (LinearLayout) findViewById(R.id.main_page_list);
-        View mView = findViewById(R.id.sburro);
-        mView.setBackgroundResource(R.drawable.walking_stickman);
-        AnimationDrawable sbu = (AnimationDrawable) mView.getBackground();
-        sbu.start();
+      mMainPageList = (LinearLayout) findViewById(R.id.main_page_list);
+      mUserView = (ImageView) findViewById(R.id.user_icon_view);
+      View mView = findViewById(R.id.walking_man);
+      mView.setBackgroundResource(R.drawable.walking_stickman);
 
 
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mDrawerElements));
-        
+      mUserView.bringToFront();
 
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            }
-        });
+      AnimationDrawable sbu = (AnimationDrawable) mView.getBackground();
+      sbu.start();
 
-        mMenuIcon = (ImageView) findViewById(R.id.action_bar_icon);
-        mMenuIcon.setOnClickListener(new View.OnClickListener() {
+      getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+      getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+      String[] mMenuStrings = getResources().getStringArray(R.array.main_page_list_items);
+
+      for(final String i: mMenuStrings) {
+
+         final View v; // Creating an instance for View Object
+         LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+         v = inflater.inflate(R.layout.menu_fragment_item, null);
+         TextView textTemp = (TextView) v.findViewById(R.id.item_text);
+         textTemp.setText(i);
+
+         v.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawerToggle();
+               launchNextActivity(i);
             }
-        });
+         });
 
-        String[] mMenuStrings = getResources().getStringArray(R.array.main_page_list_items);
+         mMainPageList.addView(v);
 
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+      }
 
-        for(final String i: mMenuStrings) {
+   }
 
-            final View v; // Creating an instance for View Object
-            LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            v = inflater.inflate(R.layout.menu_fragment_item, null);
-            TextView textTemp = (TextView) v.findViewById(R.id.item_text);
-            textTemp.setText(i);
+   private void launchNextActivity(String activityName) {
+      Intent intent = null;
+      switch (activityName) {
 
-            v.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    launchNextActivity(i);
-                }
-            });
+         case "Start":
+            intent = new Intent(this, Training.class);
+            break;
 
-            mMainPageList.addView(v);
+         case "Continue":
+            Toast.makeText(this, activityName + " non va, sburro",Toast.LENGTH_SHORT).show();
 
-        }
+            //Intent intent = new Intent(this, DisplayMessageActivity.class);
+            break;
 
-    }
+         case "Load":
+            intent = new Intent(this, LoadTrainings.class);
+            break;
 
-    private void launchNextActivity(String activityName) {
-        Intent intent = null;
-        switch (activityName) {
+      }
+      if (intent != null) {
+         startActivity(intent);
+      }
 
-            case "Start":
-                intent = new Intent(this, Training.class);
-                break;
-
-            case "Continue":
-                Toast.makeText(this, activityName + " non va, sburro",Toast.LENGTH_SHORT).show();
-
-                //Intent intent = new Intent(this, DisplayMessageActivity.class);
-                break;
-
-            case "Load":
-                intent = new Intent(this, LoadTrainings.class);
-                break;
-
-        }
-        if (intent != null) {
-            startActivity(intent);
-        }
-
-    }
-
-
-
-    private void drawerToggle () {
-
-       if ( mDrawerLayout.isDrawerOpen(GravityCompat.START) ) {
-           mDrawerLayout.closeDrawer(Gravity.LEFT);
-       } else {
-           mDrawerLayout.openDrawer(Gravity.LEFT);
-       }
-    }
+   }
 
 }
