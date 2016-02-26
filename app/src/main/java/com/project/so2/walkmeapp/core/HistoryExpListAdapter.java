2ddd79c9e5790bfb1def12c672c3fbf893b535f8
Andrea Medda <andrea.medda@live.it>
2016-02-26 17:15:40 +0100

    HistoryExp javadoc

--- app/src/main/java/com/project/so2/walkmeapp/core/HistoryExpListAdapter.java
+++ app/src/main/java/com/project/so2/walkmeapp/core/HistoryExpListAdapter.java
@@ -15,14 +15,24 @@ import com.project.so2.walkmeapp.core.ORM.DBTrainings;
 import java.util.HashMap;
 import java.util.List;
 
-
+/**
+ * Class used to Handle the right order and format of trainings stored in the db
+ */
 public class HistoryExpListAdapter extends BaseExpandableListAdapter {
 
    private Context context;
-   private List<String> listDataHeader; // header titles
-   // child data in format of header title, child title
+   /* Header titles */
+   private List<String> listDataHeader;
+   /* Child data in format of header title, child title */
    private HashMap<String, List<DBTrainings>> listDataChild;
 
+   /**
+    * Defining History's information to be shown
+    *
+    * @param context        Context used by the DB
+    * @param listDataHeader Header Titles
+    * @param listChildData  Child data
+    */
    public HistoryExpListAdapter(Context context, List<String> listDataHeader,
                                 HashMap<String, List<DBTrainings>> listChildData) {
       this.context = context;
@@ -30,17 +40,41 @@ public class HistoryExpListAdapter extends BaseExpandableListAdapter {
       this.listDataChild = listChildData;
    }
 
+   /**
+    * Handling Trainings' order in History View
+    *
+    * @param groupPosition  Training's Month
+    * @param childPosititon Month's trainings
+    * @return training's information
+    */
    @Override
    public DBTrainings getChild(int groupPosition, int childPosititon) {
       return this.listDataChild.get(this.listDataHeader.get(groupPosition))
               .get(childPosititon);
    }
 
+   /**
+    * Getting child information
+    *
+    * @param groupPosition Training's Month
+    * @param childPosition Month's Training Position
+    * @return child information
+    */
    @Override
    public long getChildId(int groupPosition, int childPosition) {
       return childPosition;
    }
 
+   /**
+    * Handling the view per each child
+    *
+    * @param groupPosition Training's Month
+    * @param childPosition Child training ordered by data per each month
+    * @param isLastChild   Used to check if it's the last child of a month
+    * @param convertView   Needed by the adapter
+    * @param parent        Child's parent
+    * @return View per each Child
+    */
    @Override
    public View getChildView(int groupPosition, final int childPosition,
                             boolean isLastChild, View convertView, ViewGroup parent) {
@@ -56,31 +90,64 @@ public class HistoryExpListAdapter extends BaseExpandableListAdapter {
       TextView txtListChild = (TextView) convertView
               .findViewById(R.id.item_text);
 
-      txtListChild.setText("ID: " + childText.id + " - Data: " + childText.date_day + "/" + childText.date_month + " - " + childText.date_hour + ":" + childText.date_minutes);
+      txtListChild.setText("ID: " + childText.id + " - Data: " + childText.date_day + "/" +
+              childText.date_month + " - " + childText.date_hour + ":" + childText.date_minutes);
       return convertView;
    }
 
+   /**
+    * Counts how many children are contained in each month
+    *
+    * @param groupPosition Month
+    * @return Number or children per month
+    */
    @Override
    public int getChildrenCount(int groupPosition) {
       return this.listDataChild.get(this.listDataHeader.get(groupPosition))
               .size();
    }
 
+   /**
+    * Getting groups
+    *
+    * @param groupPosition Month
+    * @return Group
+    */
    @Override
    public Object getGroup(int groupPosition) {
       return this.listDataHeader.get(groupPosition);
    }
 
+   /**
+    * Counts how many months store trainings' data
+    *
+    * @return Number of existing months
+    */
    @Override
    public int getGroupCount() {
       return this.listDataHeader.size();
    }
 
+   /**
+    * Getting month's id
+    *
+    * @param groupPosition month
+    * @return Month Id per each group
+    */
    @Override
    public long getGroupId(int groupPosition) {
       return groupPosition;
    }
 
+   /**
+    * Managing Views per each month
+    *
+    * @param groupPosition Month
+    * @param isExpanded    Checks if the group has been expanded or not
+    * @param convertView   Needed by adapter
+    * @param parent        Month's parent
+    * @return View per month
+    */
    @Override
    public View getGroupView(int groupPosition, boolean isExpanded,
                             View convertView, ViewGroup parent) {
@@ -99,11 +166,23 @@ public class HistoryExpListAdapter extends BaseExpandableListAdapter {
       return convertView;
    }
 
+   /**
+    * Required by adapter
+    *
+    * @return false
+    */
    @Override
    public boolean hasStableIds() {
       return false;
    }
 
+   /**
+    * Checks if a child is selectable
+    *
+    * @param groupPosition Month
+    * @param childPosition Child per month
+    * @return true
+    */
    @Override
    public boolean isChildSelectable(int groupPosition, int childPosition) {
       return true;
